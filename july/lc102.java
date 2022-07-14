332db897ce9bbb891cff63f0809da26b32cbb7e8
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-07-14 22:07:01 +0530

    434

--- july/lc102.java
+++ july/lc102.java
@@ -21,3 +21,31 @@ class Solution {
       return list;
     }
 }
+//
+ public List<List<Integer>> levelOrder(TreeNode root) {
+      LinkedList <TreeNode> q = new LinkedList < >();
+      List<List<Integer>> list = new LinkedList <List<Integer>>();
+      if(root == null)
+        return list;
+      
+      q.addLast(root);
+        while(q.size() != 0){
+            int size = q.size();
+             List<Integer> subList = new LinkedList<Integer>();
+            // subList.add(q.getFirst().val);
+            // list.add(subList);
+            while( size -- >0){
+              // List<Integer> subList = new LinkedList<Integer>();
+            subList.add(q.getFirst().val);
+               TreeNode rn = q.removeFirst();
+                if(rn.left != null){
+                    q.addLast(rn.left);
+                }
+                if(rn.right != null){
+                    q.addLast(rn.right);
+                } 
+                
+            }       list.add(subList);      
+
+
+        }

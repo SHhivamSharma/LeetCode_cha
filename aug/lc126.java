5a994a224e978ad46abd2f5195f52cf04c9ddab2
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-08-15 23:06:58 +0530

    11

--- aug/lc126.java
+++ aug/lc126.java
@@ -1,66 +1,105 @@
 class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        List<List<String>> result = new ArrayList<>();
+        Set<String> dict = new HashSet(wordList);
+        if( !dict.contains(endWord) )
+            return new ArrayList();
         
-        Set<String> hs = new HashSet<>();
+        // adjacent words for each word
+        Map<String,List<String>> adjacency = new HashMap();
+        Queue<String> queue = new LinkedList();
+        // does path exist?
+        boolean found = false;
         
-        for(String word : wordList){
-            hs.add(word);
-        }
-        
-        Queue<List<String>> queue = new LinkedList<>();
-        List<String> temp = new ArrayList<>();
-        temp.add(beginWord);
-        queue.offer(temp);
-        
-        
-        while(!queue.isEmpty()){
+        // BFS for shortest path, keep removing visited words
+        queue.offer(beginWord);
+        dict.remove(beginWord);
+                
+        while( !found && !queue.isEmpty() ) {
             int size = queue.size();
-            Set<String> eligibleToRemove = new HashSet<>();
-            
-            for(int i = 0; i < size; i++){
-                List<String> currList = queue.poll();
-                String currWord = currList.get(currList.size() - 1);
+            // adjacent words in current level
+            HashSet<String> explored = new HashSet();
+
+            while( size-- > 0 ) {
+                String word = queue.poll();
                 
-                if(currWord.equals(endWord)){
-                    result.add(new ArrayList<>(currList));
-                }
+                if( adjacency.containsKey(word) )
+                    continue;
                 
-                List<String> neighbours = findAllNeighbours(currWord, hs);
-                for(String neighbour : neighbours){
-                    currList.add(neighbour);
-                    queue.offer(new ArrayList<>(currList));
-                    currList.remove(currList.size() - 1);
-                    eligibleToRemove.add(neighbour);
+                // remove current word from dict, and search for adjacent words
+                dict.remove(word);
+                List<String> adjacents = getAdjacents(word, dict);
+                adjacency.put(word, adjacents);
+
+                for(String adj : adjacents) {
+                    if( !found && adj.equals(endWord) ) 
+                        found = true;
+                    
+                    explored.add(adj);
+                    queue.offer(adj);
                 }
             }
-            
-            for(String word : eligibleToRemove){
-                hs.remove(word);
-            }
+            // remove words explored in current level from dict
+            for(String word : explored)
+                dict.remove(word);
         }
-        
-        return result;
+
+        // if a path exist, dfs to find all the paths
+        if( found ) 
+            return dfs(beginWord, endWord, adjacency, new HashMap());
+        else
+            return new ArrayList();
     }
     
-    private List<String> findAllNeighbours(String word, Set<String> hs){
+    private List<String> getAdjacents(String word, Set<String> dict) {
+        List<String> adjs = new ArrayList();
+        char[] wordChars = word.toCharArray();
         
-        char[] wordChar = word.toCharArray();
-        List<String> neighbours = new ArrayList<>();
-        
-        for(int i = 0; i < wordChar.length; i++){
-            char ch = wordChar[i];
-            
-            for(char character = 'a'; character <= 'z'; character++){
-                    wordChar[i] = character;
-                    String eligibleNeighbour = new String(wordChar);
-                    if(hs.contains(eligibleNeighbour) && character != ch)
-                        neighbours.add(eligibleNeighbour);
+        for(int i=0; i<wordChars.length; i++) 
+            for(char c='a'; c<='z'; c++) {
+                char temp = wordChars[i];
+                wordChars[i] = c;
+                
+                String newAdj = new String(wordChars);
+                if( dict.contains(newAdj) )
+                    adjs.add(newAdj);
+                
+                wordChars[i] = temp;
             }
-            
-            wordChar[i] = ch;
-        }
+        return adjs;
+    }
+    
+    private List<List<String>> dfs(String src, String dest, 
+                                   Map<String,List<String>> adjacency, 
+                                   Map<String,List<List<String>>> memo) {
+        if( memo.containsKey(src) )
+            return memo.get(src);
         
-        return neighbours;
+        List<List<String>> paths = new ArrayList();
+        
+		// reached dest? return list with dest word
+        if( src.equals( dest ) ) {
+            paths.add( new ArrayList(){{ add(dest); }} );
+            return paths;
+        }
+
+		// no adjacent for curr word? return empty list
+        List<String> adjacents = adjacency.get(src);
+        if( adjacents == null || adjacents.isEmpty() )
+            return paths;
+
+        for(String adj : adjacents) {
+            List<List<String>> adjPaths = dfs(adj, dest, adjacency, memo);
+            
+            for(List<String> path : adjPaths) {
+                if( path.isEmpty() ) continue;
+                
+                List<String> newPath = new ArrayList(){{ add(src); }};
+                newPath.addAll(path);
+                
+                paths.add(newPath);
+            }
+        } 
+        memo.put(src, paths);
+        return paths;
     }
 }

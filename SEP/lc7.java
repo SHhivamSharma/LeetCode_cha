a2049948334247659425e5e26eae3827f36188ee
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-09-07 22:19:26 +0530

    Create lc7.java

---
+++ SEP/lc7.java
@@ -0,0 +1,13 @@
+//606. Construct String from Binary Tree
+
+public class Solution {
+    public String tree2str(TreeNode t) {
+        if(t==null)
+            return "";
+        if(t.left==null && t.right==null)
+            return t.val+"";
+        if(t.right==null)
+            return t.val+"("+tree2str(t.left)+")";
+        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";   
+    }
+}

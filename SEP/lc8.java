821e793b7e47140d5d2327899f678a187ecb63d0
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-09-08 22:43:08 +0530

    11

---
+++ SEP/lc8.java
@@ -0,0 +1,18 @@
+/// 94. Binary Tree Inorder Traversal
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
+    }
+}

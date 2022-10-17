4b5cfa945d3115bcddbecce47ec069b2e51e0fb2
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-10-17 22:17:42 +0530

    11

---
+++ oct/lc17.java
@@ -0,0 +1,15 @@
+//// 1832. Check if the Sentence Is Pangram
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        // Empty set 'seen'.
+        Set<Character> seen = new HashSet<>();
+        
+        // Iterate over 'sentence' add every letter to 'seen'.
+        for (char currChar : sentence.toCharArray())
+            seen.add(currChar);
+        
+        // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
+    }
+}

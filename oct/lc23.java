617eb8c4b258134a398b301cc488ed3075236790
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-10-28 00:05:47 +0530

    11

--- oct/lc23.java
+++ oct/lc23.java
@@ -4,6 +4,8 @@ public class Solution {
     public int[] findErrorNums(int[] nums) {
         int dup = -1, missing = -1;
         for (int i = 1; i <= nums.length; i++) {
+
+            //lmndf
             int count = 0;
             for (int j = 0; j < nums.length; j++) {
                 if (nums[j] == i)

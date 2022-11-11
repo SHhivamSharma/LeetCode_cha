04e8ce2f34673c133711d93aaf2616a092f89d80
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-11-11 23:10:57 +0530

    11

---
+++ nov/lc11.java
@@ -0,0 +1,11 @@
+//26. Remove Duplicates from Sorted Array
+
+class Solution {
+    public int removeDuplicates(int[] A) {
+        if (A.length==0) return 0;
+    int j=0;
+    for (int i=0; i<A.length; i++)
+        if (A[i]!=A[j]) A[++j]=A[i];
+    return ++j;
+    }
+}
